6dafd89a32a9a61d2490687bc160aaefaf9c18bf
Michael Jones <mj6@email.uscupstate.edu>
2017-04-06 02:02:33 +0000

    Update repository on Wed, April 5th at 10:00PM

--- config/routes.rb
+++ config/routes.rb
@@ -2,65 +2,64 @@ Rails.application.routes.draw do
   
   get 'sessions/new'
 
-  get 'users/new'
-  get 'users/show'
-
   root 'static_pages#home'
+  
+  get '/home',         to: 'static_pages#home'
+  get '/street',       to: 'static_pages#street'
+  get '/offroad',      to: 'static_pages#offroad'
+  get '/ridinggear',   to: 'static_pages#ridinggear'
+  get '/contact',      to: 'static_pages#contact'
   
   get 'static_pages/home'
   get 'static_pages/street'
+  get 'static_pages/offroad'
+  get 'static_pages/ridinggear'
+  get 'static_pages/contact'
   
-  get "/users/home.html.erb" => "static_pages#home"
+  get  '/signup',      to: 'users#new'
+  post '/signup',      to: 'users#create'
   
-  get "/home.html.erb" => "static_pages#home"
-  get "/street.html.erb" => "static_pages#street"
-  get "/offroad.html.erb" => "static_pages#offroad"
-  get "/ridinggear.html.erb" => "static_pages#ridinggear"
-  get "/show.html.erb" => "static_pages#contact"
-  get "/about.html.erb" => "static_pages#about"
-  get '/signup' => 'users#new'
-  post '/signup',  to: 'users#create'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get 'users/new'
+  get 'users/show'
+  
+  get    '/login',     to: 'sessions#new'
+  post   '/login',     to: 'sessions#create'
+  delete '/logout',    to: 'sessions#destroy'
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/delete'
 
+  get '/sport',        to: 'static_pages#sport'
+  get '/sporttouring', to: 'static_pages#sporttouring'
+  get '/supersport',   to: 'static_pages#supersport'
+  get '/cruiser',      to: 'static_pages#cruiser'
+  
   get 'static_pages/sport'
   get 'static_pages/sporttouring'
   get 'static_pages/supersport'
   get 'static_pages/cruiser'
   
-  get "/sport.html.erb" => "static_pages#sport"
-  get "/sporttouring.html.erb" => "static_pages#sporttouring"
-  get "/supersport.html.erb" => "static_pages#supersport"
-  get "/cruiser.html.erb" => "static_pages#cruiser"
+  get '/motocross',    to: 'static_pages#motocross'
+  get '/trailbike',    to: 'static_pages#trailbike'
+  get 'dualsport',     to: 'static_pages#dualsport'
   
   get 'static_pages/motocross'
   get 'static_pages/trailbike'
   get 'static_pages/dualsport'
   
-  get "/motocross.html.erb" => "static_pages#motocross"
-  get "/trailbike.html.erb" => "static_pages#trailbike"
-  get "/dualsport.html.erb" => "static_pages#dualsport"
-  
+  get '/helmets',      to: 'static_pages#helmets'
+  get '/jerseysets',   to: 'static_pages#jerseysets'
+  get '/goggles',      to: 'static_pages#goggles'
+  get '/gloves',       to: 'static_pages#gloves'
+  get '/boots',        to: 'static_pages#boots'
+
   get 'static_pages/helmets'
   get 'static_pages/jerseysets'
   get 'static_pages/goggles'
   get 'static_pages/gloves'
   get 'static_pages/boots'
-  
-  get "/helmets.html.erb" => "static_pages#helmets"
-  get "/jerseysets.html.erb" => "static_pages#jerseysets"
-  get "/goggles.html.erb" => "static_pages#goggles"
-  get "/gloves.html.erb" => "static_pages#gloves"
-  get "/boots.html.erb" => "static_pages#boots"
 
   resources :users
   
-  get 'static_pages/offroad'
-  get 'static_pages/ridinggear'
-  get 'static_pages/about'
-  
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/delete'
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,29 +1,103 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    # assert_select "title", "Home | HOUSE OF THUNDER"
   end
 
   test "should get street" do
-    get static_pages_street_url
+    get street_path
     assert_response :success
-  end
-  
-  test "should get offroad" do
-    get static_pages_offroad_url
-    assert_response :success
-  end
-  
-  test "should get ridinggear" do
-    get static_pages_ridinggear_url
-    assert_response :success
-  end
-  
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
+    # assert_select "title", "Street | HOUSE OF THUNDER"
   end
 
+  test "should get offroad" do
+    get offroad_path
+    assert_response :success
+    # assert_select "title", "Off-road | HOUSE OF THUNDER"
+  end
+
+  test "should get ridinggear" do
+    get ridinggear_path
+    assert_response :success
+    # assert_select "title", "Riding Gear | HOUSE OF THUNDER"
+  end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    # assert_select "title", "Contact | HOUSE OF THUNDER"
+  end
+  
+  test "should get sport" do
+    get sport_path
+    assert_response :success
+    # assert_select "title", "Sport | HOUSE OF THUNDER"
+  end
+  
+  test "should get sporttouring" do
+    get sporttouring_path
+    assert_response :success
+    # assert_select "title", "Sport-Touring | HOUSE OF THUNDER"
+  end
+  
+  test "should get supersport" do
+    get supersport_path
+    assert_response :success
+    # assert_select "title", "Supersport | HOUSE OF THUNDER"
+  end
+  test "should get cruiser" do
+    get cruiser_path
+    assert_response :success
+    # assert_select "title", "Cruiser | HOUSE OF THUNDER"
+  end
+  test "should get motocross" do
+    get motocross_path
+    assert_response :success
+    # assert_select "title", "Motocross | HOUSE OF THUNDER"
+  end
+  test "should get trailbike" do
+    get trailbike_path
+    assert_response :success
+    # assert_select "title", "Trailbike | HOUSE OF THUNDER"
+  end
+  
+  test "should get dualsport" do
+    get dualsport_path
+    assert_response :success
+    # assert_select "title", "Dual-sport | HOUSE OF THUNDER"
+  end
+  
+  test "should get helmets" do
+    get helmets_path
+    assert_response :success
+    # assert_select "title", "Helmets | HOUSE OF THUNDER"
+  end
+  
+  test "should get jerseysets" do
+    get jerseysets_path
+    assert_response :success
+    # assert_select "title", "Jersey Sets | HOUSE OF THUNDER"
+  end
+  
+  test "should get goggles" do
+    get goggles_path
+    assert_response :success
+    # assert_select "title", "Goggles | HOUSE OF THUNDER"
+  end
+  
+  test "should get gloves" do
+    get gloves_path
+    assert_response :success
+    # assert_select "title", "Gloves | HOUSE OF THUNDER"
+  end
+  
+  test "should get boots" do
+    get boots_path
+    assert_response :success
+    # assert_select "title", "Boots | HOUSE OF THUNDER"
+  end
 end
